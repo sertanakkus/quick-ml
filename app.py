63f69a5ed633d5f547fe1af7a80fbcecfb90a4e4
Sertan Akkuş <sertanakkus.sa@gmail.com>
2024-03-21 00:46:24 +0300

    feat: add start button to start training with selected algorithm

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 import pandas as pd
 import streamlit as st
 
+from apply_algorithm import Algorithms
 
-def show_options(cols):
-    st.container(height=20, border=False)
-    st.selectbox(
-        r"$\textsf{\large Algorithm}$",
+
+def get_options(cols):
+    st.container(height=10, border=False)
+    algorithm = st.selectbox(
+        r"$\textsf{\large Algorithm}$", list(algorithm_list.keys())
     )
-    st.container(height=20, border=False)
-    st.selectbox(r"$\textsf{\large Target variable}$", cols)
+
+    st.container(height=10, border=False)
+    target = st.selectbox(r"$\textsf{\large Target variable}$", cols)
+
+    return algorithm, target
 
 
 def main():
     st.title("Quick Machine Learning")
+    st.container(height=10, border=False)
 
     file = st.file_uploader(r"$\textsf{\large Upload a csv file}$", type="csv")
 
@@ -25,12 +31,27 @@ def main():
             new_context = context.replace(";", ",").replace("'", "").replace('"', "")
             f_out.write(new_context)
 
-            df = pd.read_csv("temp.csv")
+        df = pd.read_csv("temp.csv")
 
-            cols = df.columns
+        cols = df.columns
 
-            show_options(cols)
+        algorithm, target = get_options(cols)
 
+        if st.button("Start"):
+            result = Algorithms.apply(algorithm, target)
+
+            st.write(result)
+
+
+algorithm_list = {
+    "Linear Regression": 0,
+    "Logistic Regression": 1,
+    "Decision Tree Regressor": 2,
+    "Random Forest Regressor": 3,
+    "KNN": 4,
+    "SVR": 5,
+    "SVC": 6,
+}
 
 if __name__ == "__main__":
     main()
