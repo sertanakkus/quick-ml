aa89d8dd0fc40b64ec9cfaa7b590db0e7409250d
Sertan Akkuş <sertanakkus.sa@gmail.com>
2024-03-21 00:38:42 +0300

    add model files

---
+++ models/knn.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+from sklearn.preprocessing import LabelEncoder
+
+
+import warnings
+
+warnings.filterwarnings("ignore", "\nPyarrow", DeprecationWarning)
+
+
+def KNN(target_feature):
+    # df = pd.read_csv("../saved_file.csv")
+    df = pd.read_csv("temp.csv")
+
+    # drop na columns
+    df = df.dropna()
+
+    # encoding
+    non_numeric_columns = df.select_dtypes(exclude=["number"]).columns
+    le = LabelEncoder()
+    if target_feature in non_numeric_columns:
+        df[target_feature] = le.fit_transform(df[target_feature])
+
+    df = pd.get_dummies(data=df, columns=non_numeric_columns.drop(target_feature))
+
+    # split data
+    X = df.drop(columns=target_feature).values
+    y = df[target_feature].values
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=0
+    )
+
+    classifier = KNeighborsClassifier()
+    classifier.fit(X_train, y_train)
+
+    y_pred = classifier.predict(X_test)
+
+    score = metrics.accuracy_score(y_test, y_pred) * 100
+    precision = metrics.precision_score(
+        y_test, y_pred, pos_label="positive", average="micro"
+    )
+    recall = metrics.recall_score(y_test, y_pred, pos_label="positive", average="micro")
+    f1_score = metrics.f1_score(y_test, y_pred, pos_label="positive", average="micro")
+
+    score = round(score, 1)
+    score = f"{score} %"
+
+    return score
---
+++ models/random_forest_regressor.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import r2_score
+
+import warnings
+
+warnings.filterwarnings("ignore", "\nPyarrow", DeprecationWarning)
+
+
+def random_forest_regressor(target_feature):
+    # df = pd.read_csv("../saved_file.csv")
+    df = pd.read_csv("temp.csv")
+
+    # drop na columns
+    df = df.dropna()
+
+    # encoding
+    non_numeric_columns = df.select_dtypes(exclude=["number"]).columns
+    le = LabelEncoder()
+    if target_feature in non_numeric_columns:
+        df[target_feature] = le.fit_transform(df[target_feature])
+
+    df = pd.get_dummies(
+        data=df, columns=non_numeric_columns.drop(target_feature, errors="ignore")
+    )
+
+    # split data
+    X = df.drop(columns=target_feature).values
+    y = df[target_feature].values
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=0
+    )
+
+    regressor = RandomForestRegressor(random_state=0)
+    regressor.fit(X_train, y_train)
+
+    y_pred = regressor.predict(X_test)
+
+    score = r2_score(y_test, y_pred) * 100
+    score = round(score, 1)
+    score = f"{score} %"
+
+    return score
